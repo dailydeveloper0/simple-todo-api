ce2652e9089de8be7b11be56afcab79136876473
dailydeveloper <dailydeveloper51437@gmail.com>
2026-06-18 19:40:32 +0300

    defined the create-task dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTaskDto {}
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @MinLength(1, { message: 'Title is required!' })
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  @IsBoolean()
+  completed: boolean;
+}
